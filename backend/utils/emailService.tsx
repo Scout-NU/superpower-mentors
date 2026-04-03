373af19a11d4c7ac57d2aa58fdfaf53a6ddce10c
adrodvaz04 <rodriguezvazquez.a@northeastern.edu>
2026-04-03 10:47:37 -0400

    refactor: add quiz answer to zapier request body

--- backend/utils/emailService.tsx
+++ backend/utils/emailService.tsx
@@ -2,11 +2,17 @@
 import axios from "axios";
 import { useState } from "react";
 
-export const sendEmail = async (email: string) => {
+/**
+ * Service function to be used in frontend to send an email to the Zapier webhook.
+ * @param email the email to send
+ */
+export const sendResponse = async (email: string, quizAnswers: string[]) => {
+  const [answer1, answer2] = quizAnswers;
   try {
     await axios
       .post("/api/send-email", {
-        email: email
+        email: email,
+        quizAnswers: quizAnswers
       })
       .then((res) => {
         console.log("successful attempt", res);
@@ -18,6 +24,9 @@ export const sendEmail = async (email: string) => {
 
 export default function TestingEmail() {
   const [email, setEmail] = useState<string>("");
+  const [answer1, setAnswer1] = useState<string>("");
+  const [answer2, setAnswer2] = useState<string>("");
+  const quizAnswers = [answer1, answer2];
 
   return (
     <div id="testing-email">
@@ -33,10 +42,34 @@ export default function TestingEmail() {
       ></input>
       <br />
       <br />
+      <label htmlFor="answer1-input" className="p-2">
+        {" "}
+        Answer 1{" "}
+      </label>
+      <input
+        className="border-2 p-2"
+        type="text"
+        id="answer1-input"
+        onChange={(e) => setAnswer1(e.target.value)}
+      ></input>
+      <br />
+      <br />
+      <label htmlFor="answer1-input" className="p-2">
+        {" "}
+        Answer 2{" "}
+      </label>
+      <input
+        className="border-2 p-2"
+        type="text"
+        id="answer2-input"
+        onChange={(e) => setAnswer2(e.target.value)}
+      ></input>
+      <br />
+      <br />
       <button
         className="border-2 p-2 border-black"
         id="test-email-button"
-        onClick={() => sendEmail(email)}
+        onClick={() => sendResponse(email, quizAnswers)}
       >
         {" "}
         Send Email to Webhook{" "}
