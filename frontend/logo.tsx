32f3701becc5cd945a6eba6a05139d8be347039b
kaelynnlackeyy <lackey.ka@northeastern.edu>
2026-04-13 22:19:05 -0400

    Small UI fixes

--- frontend/logo.tsx
+++ frontend/logo.tsx
@@ -1,14 +1,13 @@
 "use client"
 
-import { Player } from "@lottiefiles/react-lottie-player"
-
+import Image from "next/image"
 export default function Logo() {
   return (
-    <Player
-      autoplay
-      loop
-      src="/animations/logo-spin.json"
-      style={{ height: "45px", width: "45px" }}
+    <Image
+      alt="Logo"
+      src="/images/Group(2).png"
+      width={45}
+      height={45}
     />
   )
 }
